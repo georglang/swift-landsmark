3b55359efe0e17b4739c046aef7f83d6aab03109
Georg Lang <mail@georglang.com>
2020-02-06 20:05:05 +0100

    renaming contentView to landsmarkDetail

---
+++ Landsmark/Landsmark/LandmarkRow.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkRow.swift
+//  Landsmark
+//
+//  Created by Georg Lang on 06.02.20.
+//  Copyright © 2020 Georg Lang. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
